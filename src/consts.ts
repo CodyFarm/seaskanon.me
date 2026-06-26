a3bd08468386b3e2482fa28c6a1939fff4ba0702
CodyFarm <302024316015@zjut.edu.cn>
2026-06-26 13:59:38 +0800

    Initialize personal website

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sorolla';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'CodyFarm';
+export const SITE_DESCRIPTION = 'seasKanon，四季的卡农。经典在变奏里重生';
