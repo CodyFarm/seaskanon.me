1bd00d945132f1292bfecd303ab7f9d22a54285a
CodyFarm <302024316015@zjut.edu.cn>
2026-07-01 11:33:49 +0800

    Add CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
 
 import icon from "astro-icon";
 
@@ -18,5 +19,17 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@/lib": fileURLToPath(new URL("./src/lib", import.meta.url)),
+        "@/consts": fileURLToPath(new URL("./src/consts.ts", import.meta.url)),
+        "@/components": fileURLToPath(new URL("./src/components", import.meta.url)),
+        "@/layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+        "@/assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+        "@/icons": fileURLToPath(new URL("./src/icons", import.meta.url)),
+        "@/i18n": fileURLToPath(new URL("./src/i18n", import.meta.url)),
+        "@/data": fileURLToPath(new URL("./src/data", import.meta.url)),
+      },
+    },
   },
 });
